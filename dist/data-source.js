b6189197e9fea54f64f5e6741080091e72050b37
Christopher Putra <christopher.putra.hartono@gmail.com>
2025-03-21 13:52:47 +0700

    Fix: data-source path

--- dist/data-source.js
+++ dist/data-source.js
@@ -1,13 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.purwadhikaDB = void 0;
 const typeorm_1 = require("typeorm");
 const config_1 = require("./config");
+const path_1 = __importDefault(require("path"));
 exports.purwadhikaDB = new typeorm_1.DataSource({
     type: "postgres",
     url: config_1.SUPABASE_URL || "",
     synchronize: false,
     logging: false,
-    entities: ["src/entities/*.ts"],
-    migrations: ["src/migrations/*.ts"]
+    entities: [path_1.default.join(__dirname, "/entities/*.ts")],
+    migrations: [path_1.default.join(__dirname, "/migrations/*.ts")]
 });
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,11 +1,12 @@
 import { DataSource } from "typeorm";
 import { SUPABASE_URL } from "./config";
+import path from "path";
 
 export const purwadhikaDB = new DataSource({
     type: "postgres",
     url: SUPABASE_URL || "",
     synchronize: false,
     logging: false,
-    entities: ["src/entities/*.ts"],
-    migrations: ["src/migrations/*.ts"]
+    entities: [path.join(__dirname,"/entities/*.ts")],
+    migrations: [path.join(__dirname,"/migrations/*.ts")]
 });

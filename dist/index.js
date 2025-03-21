bf765b9ef9686b701b47bff6daa352c270926d56
Christopher Putra <christopher.putra.hartono@gmail.com>
2025-03-21 13:45:53 +0700

    Fix: rebuild

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const data_source_1 = require("./data-source");
 const config_1 = require("./config");
-const morgan_1 = __importDefault(require("morgan"));
-const fs_1 = __importDefault(require("fs"));
-const path_1 = __importDefault(require("path"));
 const helmet_1 = __importDefault(require("helmet"));
 // import router
 const item_router_1 = __importDefault(require("./routers/item.router"));
@@ -19,9 +16,10 @@ const app = (0, express_1.default)();
 // use middleware
 app.use((0, helmet_1.default)());
 app.use(express_1.default.json());
-app.use((0, morgan_1.default)("combined", {
-    stream: fs_1.default.createWriteStream(path_1.default.join(__dirname, "access.log")),
-}));
+// app.use(morgan("combined", {
+//   stream: fs.createWriteStream(path.join(__dirname, "access.log")),
+// })
+// );
 // error handling
 app.use((err, req, res, next) => {
     res.status(500).json({ message: err.message });
